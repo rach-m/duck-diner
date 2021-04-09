1db319fabed1b30d3673b43d4db41482908d2014
RachelM <rachieb19@gmail.com>
2021-04-09 17:49:42 -0400

    CREATE

--- client/src/screens/CreateDishes/index.js
+++ client/src/screens/CreateDishes/index.js
@@ -1,7 +1,120 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getAllergens, createDish } from "../../services";
+import { useHistory } from "react-router-dom";
 
-function CreateDish(props) {
-    return <div>CREATE</div>;
+function CreateDish() {
+    let [allergens, setAllergens] = useState([]);
+    let [dish, setDish] = useState({
+        name: "",
+        price: "",
+        description: "",
+        vegetarian: "",
+        allergens: [],
+    });
+    const history = useHistory();
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    async function getData() {
+        let data = await getAllergens();
+        setAllergens(data);
+    }
+
+    function handleChange(event) {
+        let { id, value } = event.target;
+        setDish((prevState) => {
+            return { ...prevState, [id]: value };
+        });
+    }
+
+    function handleAllergenChange(event) {
+        //if checked add to array if not remove from array
+        console.dir(event.target.checked);
+        if (event.target.checked) {
+            setDish((prevState) => {
+                let arr = [...prevState.allergens, event.target.value];
+                return { ...prevState, allergens: arr };
+            });
+        } else {
+            setDish((prevState) => {
+                let index = prevState.allergens.indexOf(event.target.value);
+                let arr = prevState.allergens.slice();
+                arr.splice(index, 1);
+                return { ...prevState, allergens: arr };
+            });
+        }
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await setDish((prevState) => {
+            let price = Number(prevState.price);
+            let vegetarian = prevState.vegetarian === "true";
+
+            return { ...prevState, price, vegetarian };
+        });
+
+        await createDish(dish);
+        history.push("/");
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='name'>Name</label>
+                <input
+                    type='text'
+                    id='name'
+                    onChange={handleChange}
+                    value={dish.name}
+                />
+                <label htmlFor='price'>Price</label>
+                <input
+                    type='number'
+                    id='price'
+                    onChange={handleChange}
+                    value={dish.price}
+                />
+                <label htmlFor='description'>Description</label>
+                <input
+                    type='text'
+                    id='description'
+                    onChange={handleChange}
+                    value={dish.description}
+                />
+                <input
+                    type='radio'
+                    name='veggie'
+                    onChange={handleChange}
+                    value={true}
+                    id='vegetarian'></input>
+                <label htmlFor='vegetarian'>Is Vegetarian</label>
+                <input
+                    type='radio'
+                    name='veggie'
+                    onChange={handleChange}
+                    value={false}
+                    id='vegetarian'></input>
+                <label htmlFor='vegetarian'>Is Not Vegetarian</label>
+                {allergens.map((allergen) => {
+                    return (
+                        <div key={allergen._id}>
+                            <label htmlFor='allergen'>{allergen.name}</label>
+                            <input
+                                type='checkbox'
+                                className='allergen'
+                                value={allergen.name}
+                                onChange={handleAllergenChange}
+                            />
+                        </div>
+                    );
+                })}
+                <input type='submit' />
+            </form>
+        </div>
+    );
 }
 
 export default CreateDish;
